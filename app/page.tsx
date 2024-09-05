dfd24c8c7b6a87810560a7ffbfa7724c033ff0d5
xnabil354 <xnbl354@gmail.com>
2024-09-05 22:34:37 +0700

    fix: error

--- app/page.tsx
+++ app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
     <div className="bg-white text-gray-900 min-h-screen flex flex-col items-center justify-center">
       <main className="w-full max-w-2xl p-4">
         <TempMailGenerate />
+        
       </main>
     </div>
   );
